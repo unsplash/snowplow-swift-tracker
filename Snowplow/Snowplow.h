7692fe6517ae3ca4bb4e10c6b5cd44d2352b099b
Olivier Collet <oc@ocollet.com>
2018-04-05 12:55:01 -0400

    Create the Xcode project.

---
+++ Snowplow/Snowplow.h
@@ -0,0 +1,19 @@
+//
+//  Snowplow.h
+//  Snowplow
+//
+//  Created by Olivier Collet on 2018-04-05.
+//  Copyright © 2018 Unsplash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Snowplow.
+FOUNDATION_EXPORT double SnowplowVersionNumber;
+
+//! Project version string for Snowplow.
+FOUNDATION_EXPORT const unsigned char SnowplowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Snowplow/PublicHeader.h>
+
+
