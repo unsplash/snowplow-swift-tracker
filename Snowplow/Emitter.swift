5b7a3e0832a36649f49a482003446e0a9a08c237
Olivier Collet <oc@ocollet.com>
2025-02-20 14:36:16 -0500

    Emitter: Improve logging.

--- Snowplow/Emitter.swift
+++ Snowplow/Emitter.swift
@@ -7,25 +7,27 @@
 //
 
 import Foundation
-import os
+import os.log
 
 public actor Emitter: Sendable {
   public let payloadFlushFrequency: Int
 
   private let baseURL: String
-  private let requestMethod: RequestMethod
-  private let requestFactory: EmitterRequestFactory
+  private let logger: Logger = .init(subsystem: "SnowplowSwiftTracker", category: "Emitter")
   private let payloadStorage: PayloadStorage
-  
+  private let requestFactory: EmitterRequestFactory
+  private let requestMethod: RequestMethod
+
   public init(baseURL: String,
               requestMethod: RequestMethod = .post,
               payloadFlushFrequency: Int = 10,
               payloadPersistenceEnabled: Bool = true) {
     self.baseURL = baseURL
-    self.requestMethod = requestMethod
     self.requestFactory = .init(baseURL: baseURL)
+    self.requestMethod = requestMethod
     self.payloadFlushFrequency = payloadFlushFrequency
     self.payloadStorage = PayloadStorage(persistenceEnabled: payloadPersistenceEnabled)
+    logger.info("Emitter initialized.")
   }
   
   func input(_ payload: Payload) async {
@@ -46,39 +48,38 @@ extension Emitter {
     guard await needsFlush() else { return }
     
     do {
+      logger.info("Flushing payloads.")
       try await flush()
+      logger.info("Payloads flushed.")
     } catch {
-      os_log("%@", log: OSLog.default, type: OSLogType.error, error.localizedDescription)
+      logger.error("Failed to flush payloads: \(error).")
     }
   }
   
   private func flush() async throws {
     switch requestMethod {
     case .get:
+      logger.debug("Flushing payloads using the GET method.")
       let payloads = await payloadStorage.payloads
-      await withTaskGroup(of: Void.self) { taskGroup in
-        payloads.forEach { payload in
-          taskGroup.addTask { [self] in
-            do {
-              let request = try requestFactory.getRequest(for: payload)
-              _ = try await URLSession.shared.data(for: request)
-              await payloadStorage.remove(payloads: [payload])
-            } catch {
-              os_log("%@", log: .default, type: .error, error.localizedDescription)
-            }
+      try await withThrowingTaskGroup(of: Void.self) { group in
+        for payload in payloads {
+          group.addTask { [self] in
+            let request = try requestFactory.getRequest(for: payload)
+            _ = try await URLSession.shared.data(for: request)
+            await payloadStorage.remove([payload])
           }
+          try await group.next()
         }
       }
-      
+
     case .post:
+      logger.debug("Flushing payloads using the POST method.")
       let payloads = await payloadStorage.payloads
       let request = try requestFactory.postRequest(for: payloads)
       _ = try await URLSession.shared.data(for: request)
-      
-      await payloadStorage.remove(payloads: payloads)
+      await payloadStorage.remove(payloads)
     }
   }
-  
 }
 
 public extension Emitter {
