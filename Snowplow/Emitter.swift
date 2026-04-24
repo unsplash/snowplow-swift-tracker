7aac9dfa079cd5fe750a768f7ef8fc035a8b3c3f
Olivier Collet <oc@ocollet.com>
2026-04-24 15:26:25 -0400

    Merge PayloadStorage into Emitter.

--- Snowplow/Emitter.swift
+++ Snowplow/Emitter.swift
@@ -2,13 +2,19 @@ import Foundation
 import OSLog
 
 public actor Emitter {
-  public let payloadFlushFrequency: Int
+  let payloadFlushFrequency: Int
+
+  var storedPayloadCount: Int { payloads.count }
 
   private let baseURL: String
   private let logger: Logger = .init(subsystem: "SnowplowSwiftTracker", category: "Emitter")
-  private let payloadStorage: PayloadStorage
   private let requestFactory: EmitterRequestFactory
   private let requestMethod: RequestMethod
+
+  private let cacheFilename = "SnowplowEmitterPayloads.data"
+  private let isPersistenceEnabled: Bool
+  private var persistenceFileURL: URL?
+  private var payloads: [Payload] = []
 
   public init(baseURL: String,
               requestMethod: RequestMethod = .post,
@@ -18,30 +24,153 @@ public actor Emitter {
     self.requestFactory = .init(baseURL: baseURL)
     self.requestMethod = requestMethod
     self.payloadFlushFrequency = payloadFlushFrequency
-    self.payloadStorage = await PayloadStorage(persistenceEnabled: payloadPersistenceEnabled)
+    self.isPersistenceEnabled = payloadPersistenceEnabled
+
+    initializePersistentStorage()
 
     if Tracker.isLoggerEnabled(for: .emitter) {
       logger.info("❄️ Emitter initialized.")
     }
   }
-  
+
   func input(_ payload: Payload) async {
-    await payloadStorage.append(payload)
+    appendPayload(payload)
     await flushIfNeeded()
+  }
+
+  func removeAllStoredPayloads() {
+    if Tracker.isLoggerEnabled(for: .emitter) {
+      logger.debug("❄️ Removing \(self.storedPayloadCount) payloads.")
+    }
+
+    payloads.removeAll()
+    save()
+  }
+}
+
+// MARK: - Storage
+
+extension Emitter {
+  private func initializePersistentStorage() {
+    guard isPersistenceEnabled else {
+      if Tracker.isLoggerEnabled(for: .emitter) {
+        logger.info("❄️ Persistent storage initialized with persistent disabled.")
+      }
+      return
+    }
+
+    do {
+      let bundleId: String = Bundle.main.bundleIdentifier ?? ""
+      var url = try FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+      url.appendPathComponent("Snowplow/\(bundleId)/\(cacheFilename)")
+      persistenceFileURL = url
+
+      guard FileManager().fileExists(atPath: url.path) else {
+        if Tracker.isLoggerEnabled(for: .emitter) {
+          logger.info("❄️ Persistent storage initialized without a file.")
+        }
+        return
+      }
+
+      let encodedPayloads = try Data(contentsOf: url)
+      guard let storedPayloads = try JSONSerialization.jsonObject(with: encodedPayloads) as? [[String: Sendable]] else {
+        throw PayloadStorageError.cannotDecodeStoredData
+      }
+
+      payloads = storedPayloads.compactMap { Payload(dictionary: $0) }
+
+      if Tracker.isLoggerEnabled(for: .emitter) {
+        logger.debug("❄️ Persistent file loaded with \(self.payloads.count) payloads.")
+        logger.info("❄️ Persistent storage initialized with a file.")
+      }
+    } catch {
+      if Tracker.isLoggerEnabled(for: .emitter) {
+        logger.error("❄️ Failed to initialize the persistent file: \(error)")
+      }
+    }
+  }
+
+  private func save() {
+    guard isPersistenceEnabled else {
+      if Tracker.isLoggerEnabled(for: .emitter) {
+        logger.debug("❄️ Save canceled: persistence is disabled.")
+      }
+      return
+    }
+
+    guard let persistenceFileURL else {
+      if Tracker.isLoggerEnabled(for: .emitter) {
+        logger.error("❄️ Failed to save payloads: no persistent file URL.")
+      }
+      return
+    }
+
+    do {
+      let folderURL = persistenceFileURL.deletingLastPathComponent()
+      if FileManager.default.fileExists(atPath: folderURL.path) == false {
+        if Tracker.isLoggerEnabled(for: .emitter) {
+          logger.debug("❄️ Persistent file does not exist, creating it.")
+        }
+
+        try FileManager.default.createDirectory(at: folderURL, withIntermediateDirectories: true)
+
+        if Tracker.isLoggerEnabled(for: .emitter) {
+          logger.debug("❄️ Persistent file created.")
+        }
+      }
+
+      let payloadsToEncode = payloads.compactMap { $0.dictionaryRepresentation }
+      let encodedPayloads = try JSONSerialization.data(withJSONObject: payloadsToEncode)
+
+      if Tracker.isLoggerEnabled(for: .emitter) {
+        logger.debug("❄️ Saving \(payloadsToEncode.count) payloads.")
+      }
+
+      try encodedPayloads.write(to: persistenceFileURL, options: .atomic)
+
+      if Tracker.isLoggerEnabled(for: .emitter) {
+        logger.info("❄️ Payloads saved.")
+      }
+    } catch {
+      if Tracker.isLoggerEnabled(for: .emitter) {
+        logger.error("❄️ Failed to save to the persistent file: \(error)")
+      }
+    }
+  }
+
+  private func appendPayload(_ payload: Payload) {
+    if Tracker.isLoggerEnabled(for: .emitter) {
+      logger.debug("❄️ Adding a payload.")
+    }
+
+    payloads.append(payload)
+    save()
+  }
+
+  private func removePayloads(_ payloadsToRemove: [Payload]) {
+    if Tracker.isLoggerEnabled(for: .emitter) {
+      logger.debug("❄️ Removing \(payloadsToRemove.count) payloads.")
+    }
+
+    payloadsToRemove.forEach { payload in
+      guard let index = payloads.firstIndex(of: payload) else { return }
+      payloads.remove(at: index)
+    }
+
+    save()
   }
 }
 
 // MARK: - Flush
 
 extension Emitter {
-  private func needsFlush() async -> Bool {
-    let payloadCount = await payloadStorage.payloadCount
-    return requestMethod == .get || payloadCount >= payloadFlushFrequency
+  private func needsFlush() -> Bool {
+    requestMethod == .get || payloads.count >= payloadFlushFrequency
   }
-  
+
   private func flushIfNeeded() async {
-    guard await needsFlush() else { return }
-    
+    guard needsFlush() else { return }
+
     do {
       if Tracker.isLoggerEnabled(for: .emitter) {
         logger.info("❄️ Flushing payloads.")
@@ -66,12 +195,10 @@ extension Emitter {
         logger.debug("❄️ Flushing payloads using the GET method.")
       }
 
-      let payloads = await payloadStorage.payloads
-
       for payload in payloads {
         let request = try requestFactory.getRequest(for: payload)
         _ = try await URLSession.shared.data(for: request)
-        await payloadStorage.remove([payload])
+        removePayloads([payload])
       }
 
     case .post:
@@ -79,11 +206,10 @@ extension Emitter {
         logger.debug("❄️ Flushing payloads using the POST method.")
       }
 
-      let payloads = await payloadStorage.payloads
       let request = try requestFactory.postRequest(for: payloads)
 
       _ = try await URLSession.shared.data(for: request)
-      await payloadStorage.remove(payloads)
+      removePayloads(payloads)
     }
   }
 }
@@ -93,4 +219,8 @@ public extension Emitter {
     case get
     case post
   }
+}
+
+public enum PayloadStorageError: Error {
+  case cannotDecodeStoredData
 }
--- Snowplow/LogCategory.swift
+++ Snowplow/LogCategory.swift
@@ -3,7 +3,6 @@ import Foundation
 public enum LogCategory: CaseIterable, Sendable {
   case emitter
   case session
-  case storage
   case tracker
 }
 
